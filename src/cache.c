thehyve/davfs2

commit c883aa123b8699dbc5a22e120020a4c30a08c296

Optimize server side file size change detection
Commit 43ef5ac7b2f2 introduces performance issue, as the
update_cache_file is downloading the whole file on each lookup. It is
enough to get the latest file size when a file is not open or not dirty
to detect if it has been changed on the server, so next FUSE read will
have the correct size.

(Fixes 43ef5ac7b2f2 Always update cached file on dav_lookup)

src/cache.c
@@ -1021,9 +1021,21 @@ dav_lookup(dav_node **nodep, dav_node *parent, const char *name, uid_t uid)
         if (!(*nodep)->utime)
             update_directory(*nodep, retry);
     } else {
-        update_cache_file(*nodep);
+
         if (is_open(*nodep))
             attr_from_cache_file(*nodep);
+        else if (!is_dirty(*nodep)) {
+            int ret;
+
+            dav_props *props = NULL;
+            ret = dav_get_collection((*nodep)->path, &props);
+
+            if (ret == 0 && ((*nodep)->size != props->size)) {
+                (*nodep)->size = props->size;
+                delete_cache_file(*nodep);
+                dav_delete_props(props);
+           }
+        }
     }
 
     return 0;

commit 86a3e687e760bfa1ac05f7f3e5bad71f27e68486

Fix memory leak introduced by c883aa123b86
(Fixes c883aa123b86 Optimize server side file size change detection)

src/cache.c
@@ -1030,11 +1030,13 @@ dav_lookup(dav_node **nodep, dav_node *parent, const char *name, uid_t uid)
             dav_props *props = NULL;
             ret = dav_get_collection((*nodep)->path, &props);
 
-            if (ret == 0 && ((*nodep)->size != props->size)) {
-                (*nodep)->size = props->size;
-                delete_cache_file(*nodep);
+            if (ret == 0) {
+                if ((*nodep)->size != props->size) {
+                    (*nodep)->size = props->size;
+                    delete_cache_file(*nodep);
+                }
                 dav_delete_props(props);
-           }
+            }
         }
     }
 